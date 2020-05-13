3a70ab660a47c065bb23515fcf64000599666dc2
Matt Montgomery <mmontgomery@deseretdigital.com>
2020-05-13 13:47:57 -0600

    More fixes for styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const PostsWrapper = styled.div`
   display: grid;
   grid-template-columns: auto;
   grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
   @media (min-width: 900px) {
     grid-template-columns: repeat(3, minmax(400px, 800px));
   }
